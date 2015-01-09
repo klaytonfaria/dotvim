5b489a338705e0276a7cf3148169553f81857d74
Klayton Faria <klayton.souza@gmail.com>
2015-01-09 16:22:59 -0200

    Update installer

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-ln -s ~/dotvim ~/.vim
-ln -s ~/dotvim/vimrc ~/.vimrc
+rm -rf ~/\.vim && ln -s ~/dotvim ~/.vim
+rm -rf ~/\.vimrc && ln -s ~/dotvim/vimrc ~/.vimrc
 
 git submodule init
 git submodule update
+echo "\x1b[33m\o/ Awesome\! \x1b[32mYour vim setup has been finished\!"
