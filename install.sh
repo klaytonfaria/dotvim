ab7a11e6e0f13824c99958150c97d36464ea075f
Klayton Faria <klayton.souza@gmail.com>
2015-01-09 16:28:08 -0200

    Fix message text

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@ rm -rf ~/\.vimrc && ln -s ~/dotvim/vimrc ~/.vimrc
 
 git submodule init
 git submodule update
-echo "\x1b[33m\o/ Awesome\! \x1b[32mYour vim setup has been finished\!"
+echo "\x1b[33m\o/ Awesome! \x1b[32mYour vim setup has been finished!"
